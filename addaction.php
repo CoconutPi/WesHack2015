40d947168f674d45d90fe30502823e7c67e5c4fe
CoconutPi <RapiSongkunnatham@Rapis-MBP.wireless.wesleyan.edu>
2015-11-08 10:55:49 -0500

    p

---
+++ addaction.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<?php echo $_POST["coursecode"]; ?><br>
+took it <?php echo $_POST["year"]; ?><br>
+with <?php echo $_POST["professor"]; ?><br>
+got a <?php echo $_POST["grade"]; ?><br>
+
+</body>
+</html>
