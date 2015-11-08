af02441381f85a571af0a47c81a080f365b764f9
sonicnehe <lonicolasfernandez@gmail.com>
2015-11-08 00:56:25 -0500

    More changes

--- main.py
+++ main.py
@@ -10,39 +10,42 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-
+entryDict = []
 # Define a Post model for the Datastore
 class Post(ndb.Model):
-    title = ndb.StringProperty(required=True)
-    author = ndb.StringProperty(required=True)
-    content = ndb.TextProperty(required=True)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-    comment_keys = ndb.KeyProperty(kind='Comment', repeated=True)
+	courseCode = ndb.StringProperty(required=True)
+	year = ndb.StringProperty(required=True)
+	professor = ndb.StringProperty(required=True)
+	grade = ndb.StringProperty(required=True)
+	courseReview = ndb.TextProperty(required=True)
+	professorReview = ndb.TextProperty(required=True)
+	date = ndb.DateTimeProperty(auto_now_add=True)
 
-class Comment(ndb.Model):
-    name = ndb.StringProperty(required=True)
-    comment = ndb.TextProperty(required=True)
-    date = ndb.DateTimeProperty(auto_now_add=True)
 
 class ForumHandler(webapp2.RequestHandler):
     def get(self):
         # Get all of the student data from the datastore
         query = Post.query()
-        post_data = query.fetch()
+        filtered_query.filter(Post.courseCode)
+
+        post_data = filtered_query.fetch()
         # Pass the data to the template
         template_values = {
-            'posts' : post_data
+            'course_posts' : post_data
         }
         template = JINJA_ENVIRONMENT.get_template('forum.html')
         self.response.write(template.generate(template_values))
 
     def post(self):
         # Get the student name and university from the form
-        title = self.request.get('title')
-        content = self.request.get('content')
-        author = self.request.get('author')
+        courseCode = self.request.get('courseCode')
+        year = self.request.get('year')
+        professor = self.request.get('professor')
+        grade = self.request.get('grade')
+        courseReview = self.request.get('courseReview')
+        professorReview = self.request.get('professorReview')
         # Create a new Student and put it in the datastore
-        post = Post(title=title, content=content, author=author)
+        post = Post(courseCode=courseCode, year=year, professor=professor, grade=grade, courseReview=courseReview, professorReview = professorReview)
         post.put()
         # Redirect to the main handler that will render the template
         self.redirect('/forum')
